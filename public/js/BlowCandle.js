429f97b50ddbcdf55d594cd7eff037d9b150d397
hassi00 <hasibiz5566@gmail.com>
2024-02-12 22:54:25 +0900

    吹き消す処理の修正。

--- public/js/BlowCandle.js
+++ public/js/BlowCandle.js
@@ -10,6 +10,7 @@ export function BlowCandle() {
       const bufferLength = analyser.frequencyBinCount;
       const dataArray = new Uint8Array(bufferLength);
 
+      alert('マイクに向かって吹きかけて！');
       // 音声入力の監視を開始
       const checkAudio = function () {
         analyser.getByteFrequencyData(dataArray);
@@ -21,8 +22,20 @@ export function BlowCandle() {
 
         // 音量が一定レベル以上なら炎を消す
         if (average > 20) { // 閾値は実際の環境に応じて調整
-          document.getElementById('flame').style.display = 'none';
+          const flames = document.querySelectorAll('.flame');
+
+          flames.forEach(function (flame, index) {
+            // 各炎に対して、インデックスに応じた遅延を設定
+            setTimeout(function () {
+              flame.style.opacity = '0'; // 透明度を0にする
+              flame.style.transform = 'scale(0)'; // サイズを0に縮小
+              setTimeout(function () {
+                flame.style.display = 'none'; // 完全に非表示にする
+              }, 1000); // 縮小と透明化のアニメーションが完了するのを待つ
+            }, index * 1000); // 各炎が前の炎が消えてから1秒後に消えるように設定
+          });
           audioContext.close(); // リソース解放
+          StopAudioStream(stream);
         } else {
           requestAnimationFrame(checkAudio); // 音声監視を続ける
         }
@@ -30,11 +43,12 @@ export function BlowCandle() {
       checkAudio();
     })
     .catch(function (err) {
-        const button = document.getElementById('blow-candle');
-        var buttonText = button.textContent;
-        alert('「${buttonText}」ボタンを長押ししてね！');
-        // 長押しで炎を消すイベントリスナーを設定
-        setupLongPressToBlowOutCandle();
+      const button = document.getElementById('blow-candle');
+      var buttonText = button.textContent;
+      alert('「${buttonText}」ボタンを長押ししてね！');
+      // 長押しで炎を消すイベントリスナーを設定
+      setupLongPressToBlowOutCandle();
+      
     });
 
 
@@ -48,7 +62,18 @@ function setupLongPressToBlowOutCandle() {
   button.addEventListener('mousedown', function () {
     // 長押しを検出するタイマーを設定
     timerId = setTimeout(function () {
-      document.getElementById('flame').style.display = 'none'; // 炎を消す
+      const flames = document.querySelectorAll('.flame');
+
+      flames.forEach(function (flame, index) {
+        // 各炎に対して、インデックスに応じた遅延を設定
+        setTimeout(function () {
+          flame.style.opacity = '0'; // 透明度を0にする
+          flame.style.transform = 'scale(0)'; // サイズを0に縮小
+          setTimeout(function () {
+            flame.style.display = 'none'; // 完全に非表示にする
+          }, 1000); // 縮小と透明化のアニメーションが完了するのを待つ
+        }, index * 1000); // 各炎が前の炎が消えてから1秒後に消えるように設定
+      });
     }, 2000); // 2秒間の長押しで炎を消す
   });
 
@@ -58,4 +83,9 @@ function setupLongPressToBlowOutCandle() {
       clearTimeout(timerId);
     });
   });
+}
+
+
+function StopAudioStream(stream){
+  stream.getTracks().forEach(track => track.stop());
 }
